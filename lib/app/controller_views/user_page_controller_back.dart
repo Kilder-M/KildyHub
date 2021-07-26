e1c23016c40508d34c5aa7e9b0a4f34d3599797b
Kilder <kilder.vsp@hotmail.com>
2021-07-26 17:02:39 -0300

    Repository List criado

--- lib/app/controller_views/user_page_controller_back.dart
+++ lib/app/controller_views/user_page_controller_back.dart
@@ -28,9 +28,7 @@ abstract class _UserPageControllerBack with Store{
     getUser();
   }
 
-  void a(){
-    
-  }
+  
   
 
 
--- lib/app/views/user_page.dart
+++ lib/app/views/user_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:git_api/app/controller_views/user_page_controller_back.dart';
 import 'package:git_api/app/model/user_model.dart';
+import 'package:git_api/app/widgets/list_repositories.dart';
 import 'package:git_api/app/widgets/user_card.dart';
+import 'package:git_api/main.dart';
 
 class UserPage extends StatelessWidget {
   @override
@@ -23,13 +25,21 @@ class UserPage extends StatelessWidget {
                     return Container();
                   } else {
                     UserModel user = future.data as UserModel;
-                    
+
                     return Padding(
                       padding: EdgeInsets.all(10),
                       child: SafeArea(
                         child: Column(
                           children: [
-                            UserCard(container: container(user.avatarUrl),user: user,)
+                            UserCard(
+                              container: container(user.avatarUrl),
+                              user: user,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            RepositoryList(user: user,)
+                            
                           ],
                         ),
                       ),
@@ -38,7 +48,6 @@ class UserPage extends StatelessWidget {
                 })));
   }
 }
-
 
 Widget container(String url) {
   return Container(
